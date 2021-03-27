d24b606850e1537b9701acd2064b816bad8b195c
ShahYashDilip <yashshah3010@gmail.com>
2021-03-27 10:44:51 +0530

    2703.2

--- section 7/app.py
+++ section 7/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("jinja2.html")
+    return render_template(
+        "jinja2.html", name="Yash", template_name="Jinja2")
 
 
 app.run(debug=True, port=5000)
