a19266e4c43f36a4eece2423d7f31dc64ac9327c
ShahYashDilip <yashshah3010@gmail.com>
2021-03-31 12:14:08 +0530

    3103.4

--- section 7/data_structures.py
+++ section 7/data_structures.py
@@ -2,10 +2,33 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+class GalileanMoons:
+    def __init__(self, first, second, third, fourth) -> None:
+        self.first = first
+        self.second = second 
+        self.third = third 
+        self.fourth = fourth 
+
+moons = GalileanMoons("Io", "Europa", "Ganymede", "Callisto")
+
 
 @app.route('/data-structures/')
-def datastructures():
-    movies = ["Interstellar", "Tenet", "Shutter Island"]
-    return render_template("data_structures.html", movies=movies)
+def data_structures():
+    movies = [
+        "Interstellar", 
+        "Tenet", 
+        "The sixth sense"
+    ]
+    dic = {
+        1 : "one", 
+        2 : "two"
+    }
+
+    kwargs = {
+        "movies" : movies, 
+        "dic" : dic, 
+        "moons" : moons 
+    }
+    return render_template("data_structures.html", **kwargs)
 
 app.run(port=5000)
