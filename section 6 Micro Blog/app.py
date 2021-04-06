80f970e670608caafd3e3b915b5607c65c18cd3d
ShahYashDilip <yashshah3010@gmail.com>
2021-04-06 13:19:02 +0530

    0604.2

--- section 6 Micro Blog/app.py
+++ section 6 Micro Blog/app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+entries = []
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%d-%m-%Y")
+        entries.insert(0, (entry_content, formatted_date))
+
+    entries_with_date = [
+        (
+            entry[0], 
+            entry[1], 
+            datetime.datetime.strptime(entry[1], "%d-%m-%Y").strftime("%b %d")
+        )
+        for entry in entries
+    ]
+    return render_template('index.html', entries=entries_with_date)
 
 app.run(port=5000)
